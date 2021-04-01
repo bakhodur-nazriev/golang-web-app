379737aa4147d1971515b7b1fec8e7bed26c6890
Bakhodur.Nazriev <bakhodur.nazriev@gmail.com>
2021-04-01 23:07:09 +0500

    Improve rendering templates, add config and templatedata.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"fmt"
+	"github.com/bakhodur-nazriev/modernWebApp/pkg/config"
 	"github.com/bakhodur-nazriev/modernWebApp/pkg/handlers"
+	"github.com/bakhodur-nazriev/modernWebApp/pkg/render"
+	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	var app config.AppConfig
 
-	fmt.Println("Starting application on port :8000")
-	http.ListenAndServe(":8000", nil)
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Cannot create template cache", err)
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplate(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
+
+	fmt.Println("Starting application on port :8080")
+	http.ListenAndServe(":8080", nil)
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -3,6 +3,8 @@ package render
 import (
 	"bytes"
 	"fmt"
+	"github.com/bakhodur-nazriev/modernWebApp/pkg/config"
+	"github.com/bakhodur-nazriev/modernWebApp/pkg/models"
 	"html/template"
 	"log"
 	"net/http"
@@ -11,23 +13,42 @@ import (
 
 var functions = template.FuncMap{}
 
+var app *config.AppConfig
+
+/* NewTemplates sets the config for the new package */
+func NewTemplate(a *config.AppConfig) {
+	app = a
+}
+
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+
+	return td
+}
+
 /* RenderTemplate renders templates using html/template */
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	tc, err := CreateTemplateCache()
-	if err != nil {
-		log.Fatal(err)
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+
+	var tc map[string]*template.Template
+
+	if app.UseCache {
+		// Get the template cache from the app config
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
 	}
 
 	t, ok := tc[tmpl]
 	if !ok {
-		log.Fatal(err)
+		log.Fatal("Could not get template from template cache")
 	}
 
 	buf := new(bytes.Buffer)
 
-	_ = t.Execute(buf, nil)
+	td = AddDefaultData(td)
 
-	_, err = buf.WriteTo(w)
+	_ = t.Execute(buf, td)
+
+	_, err := buf.WriteTo(w)
 	if err != nil {
 		fmt.Println("Error writing template to browser", err)
 	}
